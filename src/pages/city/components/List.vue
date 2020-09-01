e6ca6b0f6814e4ecea26d0dae43b2978397e81ff
zhangqiang <15229203224@163.com>
2020-09-01 20:36:04 +0800

    通过axios获取城市选择页面数据

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -7,44 +7,24 @@
           <div class="button-wrapper">
             <div class="button">北京</div>
           </div>
-          <div class="button-wrapper">
-            <div class="button">北京</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">北京</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">北京</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">北京</div>
-          </div>
         </div>
       </div>
       <div class="area">
         <div class="title border-topbottom">热门城市</div>
         <div class="button-list">
-          <div class="button-wrapper">
-            <div class="button">西安</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">西安</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">西安</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">西安</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">西安</div>
+          <div class="button-wrapper" v-for="item of hot" :key="item.id">
+            <div class="button">{{item.name}}</div>
           </div>
         </div>
       </div>
-      <div class="area">
+      <div class="area" v-for="(item,key) of cities" :key="key">
+        <div class="title border-topbottom">{{key}}</div>
         <div class="item-list">
-          <div class="item border-bottom" >
-            安阳
+          <div class="item border-bottom" 
+            v-for="innerItem of item" 
+            :key="innerItem.id"
+          >
+              {{innerItem.name}}
           </div>
         </div>
       </div>
@@ -56,7 +36,12 @@
 import BScroll from 'better-scroll'
 export default {
   name: 'CityList',
+  props: {
+      hot:Array,
+      cities:Object
+  },
   mounted () {
+    // 获取dome的方式
     this.scroll = new BScroll(this.$refs.wrapper)
   }
 }
