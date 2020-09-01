c2149de132fc58793e73a0f15370b2f5c299a01d
zhangqiang <15229203224@163.com>
2020-09-01 19:59:13 +0800

    城市选择页面滚动插件的使用

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="list">
+  <div class="list" ref="wrapper">
     <div>
       <div class="area">
         <div class="title border-topbottom">当前城市</div>
@@ -53,8 +53,12 @@
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
-  name: 'CityList'
+  name: 'CityList',
+  mounted () {
+    this.scroll = new BScroll(this.$refs.wrapper)
+  }
 }
 </script>
 
